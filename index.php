c4491a5b18170bce51f1e1f0e11a32047a81f21e
Gustavo Mendonca <gustavoh.men@hotmail.com>
2016-12-02 00:33:02 +0000

    Index

--- index.php
+++ index.php
@@ -10,81 +10,11 @@
 
     <title>GERCI Login</title>
 
-    <!-- Bootstrap Core CSS -->
     <link href="./resources/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="./resources/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="./resources/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="./resources/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 
 <body>
-    <script>
-        $(document).ready(function () {
-            $("#form-logar").on('submit', function (e) {
-                $('#logar-loading').css('display', 'inline');
-                e.preventDefault();
-                $.ajax({
-                    type: "POST",
-                    url: "../controllers/LoginController.php",
-                    data: new FormData(this),
-                    contentType: false,
-                    processData: false,
-                    cache: false,
-                    success: function (response) {
-                        if (response == 'success') {
-                            $("#mensagem").removeClass("alert-danger alert-warning");
-                            $("#mensagem").addClass("alert alert-success");
-                            $("#mensagem").html("Logado com sucesso!");
-                            window.location.href = "./index.php";
-                        }
-                        else if (response == 'error') {
-                            $("#mensagem").removeClass("alert-success alert-warning");
-                            $("#mensagem").addClass("alert alert-danger");
-                            $("#mensagem").html("Usuário ou senha incorretos.");
-                            $('#logar-loading').css('display', 'none');
-                        }
-                        else if (response == 'empty') {
-                            $("#mensagem").removeClass("alert-success alert-danger");
-                            $("#mensagem").addClass("alert alert-warning");
-                            $("#mensagem").html("Preencha todos os campos.");
-                            $('#logar-loading').css('display', 'none');
-                        }
-                        else if(response.indexOf("Array")!==-1){
-                            $("#mensagem").removeClass("alert-success alert-warning");
-                            $("#mensagem").addClass("alert alert-danger");
-                            $("#mensagem").html("Mude pelo menos um campo.");
-                            $('#logar-loading').css('display', 'none');
-                        }
-                        else{
-                            $("#mensagem").removeClass("alert-success alert-warning");
-                            $("#mensagem").addClass("alert alert-danger");
-                            $("#mensagem").html(response);
-                            $('#logar-loading').css('display', 'none');
-                        }
-                    }
-                });
-                return false;
-            });
-        });
-    </script>
     
     <div class="container">
         <div class="row">
@@ -108,9 +38,9 @@
                                     </label>
                                 </div>
                                 <!-- Change this to a button or input when using this as a form -->
-                                <input type="submit" class="btn btn-lg btn-success btn-block" value="Entrar">
-
-   
+                                <!--<input type="submit" class="btn btn-lg btn-success btn-block" value="Entrar">-->
+                                <a href="views/index.php" class="btn btn-lg btn-success btn-block">Entrar</a>
+                                <p id="mensagem"></p>
                             </fieldset>
                         </form>
                     </div>
@@ -125,22 +55,6 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="./resources/js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="./resources/js/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="./resources/js/sb-admin-2.js"></script>
-
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
 
 </body>
 
