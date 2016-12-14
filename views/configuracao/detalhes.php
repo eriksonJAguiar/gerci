f1e836b19b336c73b45381f921e3334956703852
Matheus Bento <knightadm@hotmail.com>
2016-12-14 11:57:38 -0200

    Add files via upload

---
+++ views/configuracao/detalhes.php
@@ -0,0 +1,6 @@
+<?php
+include_once "../templates/header.php";
+echo $_GET['ip'];
+?>
+
+<?php include_once "../templates/footer.php"; ?>
--- views/configuracao/index.php
+++ views/configuracao/index.php
@@ -1,12 +1,54 @@
-<?php include('../templates/header.php'); ?>
+<?php 
+include_once "../templates/header.php"; 
+include_once "../../model/Dispositivo.php";
+include_once "../../dao/GenericDAO.php";
+include_once "../../controllers/configuracao/C_Dispositivos.php";
+?>
+<style>
+    tr:hover{
+        cursor: hand;
+    }
+</style>
 
-	<!-- Início do container -->
-	<div class="">
-		<br/>
-		<div class="alert alert-info">
- 			 <strong>Info!</strong> Seu código vai aqui!
-		</div>
-	</div>
-	<!-- Fim do container -->
+<script>
+    function verDispositivo(ip){
+        window.location.href = "./detalhes.php?ip=" + ip;
+    }
+</script>
 
-<?php include('../templates/footer.php'); ?>
+<div class="" style="padding-top: 15px">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <td><h3>IP</h3></td>
+                <td><h3>Local</h3></td>
+                <td><h3>Descrição</h3></td>
+				<td><h3>Status</h3></td>
+            </tr>
+            <tbody>
+            <?php
+			$dispositivos = pegarDispositivos();
+            foreach($dispositivos as $dispositivo){
+				$status = @statusDispositivo($dispositivo->getIp(), "public");
+			if(empty($status)){
+				$status = "<img src='img/offline2.png'\>";
+			}else{
+				$status = "<img src='img/online2.png'\>";
+			}
+            ?>
+                <tr class="<?=$cor?>" onMouseOver="this.style.cursor='pointer'" onclick="verDispositivo('<?=$dispositivo->getIp()?>')">
+                    <td><h4><?=$dispositivo->getIp()?></h4></td>
+                    <td><h4><?=$dispositivo->getLocal()?></h4></td>
+                    <td><h4><?=$dispositivo->getDescricao()?></h4></td>
+					<td><h4><?=$status?></h4></td>
+                </tr>
+                
+            <?php
+            }
+            ?>
+            </tbody>
+        </thead>
+    </table>
+</div>
+
+<?php include_once "../templates/footer.php"; ?>
