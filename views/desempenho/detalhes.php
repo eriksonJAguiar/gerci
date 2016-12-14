fd393a1802d355a396c75c336c184e82f2a5fdc5
viniciushfr <viniciushenrique34@hotmail.com>
2016-12-14 17:15:02 -0200

    djfvnje

--- views/desempenho/detalhes.php
+++ views/desempenho/detalhes.php
@@ -1,6 +1,11 @@
 <?php
 include_once "../templates/header.php";
 echo $_GET['ip'];
+
+
+echo("jdnfvijndefiv".snmp2_get($_GET['ip'], "public", "1.3.6.1.2.1.11.1"));
+echo("sidfvbisdfv".snmp2_get($_GET['ip'], "public", "1.3.6.1.2.1.11.2"));
+
 ?>
 
 <?php include_once "../templates/footer.php"; ?>
